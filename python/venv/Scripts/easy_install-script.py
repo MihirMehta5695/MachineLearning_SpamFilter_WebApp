23c44cd972f477b298e5a891b895c4d13dd497c6
Mihir <mihirm5695@gmail.com>
2020-02-05 17:27:04 +0530

    Basic Rest API Structure

---
+++ python/venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!C:\Mihir\Projects\My\ML_Spam_Filter_Web_App\MachineLearning_SpamFilter_WebApp\python\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+    )
